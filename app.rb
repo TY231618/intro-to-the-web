93daae4514631401732f121c14a137ac8bdf419d
Tony Young <ty_training@hotmail.co.uk>
2015-12-07 16:21:32 +0000

    Added views folder with index.erb

--- app.rb
+++ app.rb
@@ -1,20 +1,6 @@
 require 'sinatra'
-require 'shotgun'
 
-get '/' do
-  "Nice"
-end
-
-get '/' do
-  "BYE"
-end
-
-get '/secret' do
-  "I have no idea what I am doing !!!"
-end
 
 get '/cat' do
-  "<div style='border: 2px dashed'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
