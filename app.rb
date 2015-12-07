74e74a08eb315ca63fa08f985cb09299511f6937
Tony Young <ty_training@hotmail.co.uk>
2015-12-07 18:09:14 +0000

    added cat-form and post route

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require 'sinatra'
 
 
-get '/random-cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
+# get '/random-cat' do
+#   @name = ["Amigo", "Oscar", "Viking"].sample
+#   erb(:index)
+# end
+
+get '/cat_form' do
+  erb :cat_form
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
 end
